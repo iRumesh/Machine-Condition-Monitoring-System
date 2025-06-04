64e6c20908fb5b53c70ced843eaf1670c9668713
irumesh <irumesh.work@gmail.com>
2025-06-04 16:13:51 +0530

    Sending data from 4G Dev kit, data sent every 1 sec to thingsboard

--- src/main.cpp
+++ src/main.cpp
@@ -8,37 +8,17 @@ Description: System includes
                 1 x IR Temperature Sensor
                 Data sent every 1 second to ThingsBoard
                 4 Tasks running on ESP32
-                Sending Data over WiFi
-Last updated date : 02/06/2025
+                Sending Data over 4G
+Last updated date : 04/06/2025
 Author: 
 
 *============================================================/
 
 /*--------Libraries------------*/
-#include "Arduino.h"
+#include <IIOTDEVKIT4G.h>
 #include "max6675.h"
-#include <WiFi.h>
-#include <PubSubClient.h>
-#include <config.h>
-
-//========Print Macro========================================/
-#define DEBUG 1
-
-#if DEBUG == 1
-#define debug(x) Serial.print(x)
-#define debugln(x) Serial.println(x)
-#define debugf(...) Serial.printf(__VA_ARGS__)
-#else
-#define debug(x)
-#define debugln(x)
-#define debugf(...)
-#endif
-
-//===========================================================/
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-/*----PIN ASSIGNING------*/
+#include "Arduino.h"
+/*----PIN ASSIGHNING------*/
 #define CT1_PIN 33 
 #define CT2_PIN 34
 
@@ -74,86 +54,144 @@ volatile float Ip2 = 0.0;
 volatile float temperatureC = 0.0;
 volatile float irTemperature = 0.0;
 
-unsigned long lastSendTime = 0;
+/*-----------MQTT configuration-------------*/
+#define Debug 0
+IIOTDEVKIT4G IIOT_Dev_kit;
 
-#define _WIFI_CONNECT 1
-#define _MQTT_CONNECT 2
-#define _SEND_DATA 3
-#define _DISCONNECT 4
+//========================================
+bool G_start_connect() {
+  uint8_t start_count = 0;
+  bool init = false;
+  do {
+    init = IIOT_Dev_kit.Init(115200);
+    start_count++;
+  } while (!init && start_count < 5);
 
-int state;
-int error_count = 0;
-#define Error_count_th 5
+  if (!init) {
+    Serial.println("init failed");
+    return false;
+  }
 
-// Function Declarations
+  long starttime = millis();
+  while (!IIOT_Dev_kit.IS_ATTACH() && millis() - starttime < 20000) {
+    Serial.print(".");
+    delay(10);
+  }
+  if (!IIOT_Dev_kit.IS_ATTACH()) {
+    Serial.println("PDP attach error");
+    return false;
+  }
+
+  Serial.println("PDP attached");
+  return true;
+}
+//========================================
+Broker TB_Broker0;
+
+#define MQTT_SERVER "thingsboard.cloud"
+#define MQTT_PORT "1883"
+#define MQTT_CLIENTID "DW1"
+#define MQTT_USERNAME "D03"
+#define MQTT_TELE_TOPIC "v1/devices/me/telemetry"
+
+bool connected = false;
+bool MQTT_STARTED = false;
+
+#define _ON_Init_4G 1
+#define _MQTT_START 2
+#define _MQTT_Connect 3
+
+#define _send_data 4
+#define _MQTT_Disconnect 5
+#define _MQTT_Stop 6
+#define _off_4G 7
+
+#define _fst_send_data 8
+
 void Task1code( void * Parameters );
 void Task2code( void * Parameters );
 void Task3code( void * Parameters );
 void Task4code( void * Parameters );
 
+int state;
 
-void setupWiFi() {
-  WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  debug("Connecting to WiFi");
-  int retries = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    debug(".");
-    retries++;
-    if (retries > 20) break;
-  }
-  debugln(WiFi.localIP());
-}
+int error_count = 0;
+#define Error_count_th 5
 
-void setupMQTT() {
-  client.setServer(MQTT_SERVER, MQTT_PORT);
-}
+unsigned long lastSendTime = 0;
+const unsigned long sendInterval = 1000;
 
-void reconnectMQTT() {
-  while (!client.connected()) {
-    debug("Attempting MQTT connection...");
-    if (client.connect(MQTT_CLIENTID, MQTT_USERNAME, NULL)) {
-      debugln("connected");
-    } else {
-      debug("failed, rc=");
-      debug(client.state());
-      delay(1000);
-    }
-  }
-}
-
-void setup() {
+void setup(){
   Serial.begin(115200);
   delay(1000);
-  debugln("Starting...");
-  state = _WIFI_CONNECT;
 
-  xTaskCreatePinnedToCore(Task1code, "CT1", 10000, NULL, 1, &CT1, 1);
-  xTaskCreatePinnedToCore(Task2code, "CT2", 10000, NULL, 1, &CT2, 1);
-  xTaskCreatePinnedToCore(Task3code, "Thermo", 10000, NULL, 1, &Thermo, 1);
-  xTaskCreatePinnedToCore(Task4code, "IR", 10000, NULL, 1, &IR, 1);
+  Serial.println("Starting......");
+  IIOT_Dev_kit.PWRDOWN();
+  delay(1000);
+
+  state = _ON_Init_4G;
+  delay(100);
+
+  xTaskCreatePinnedToCore(
+    Task1code, /* Task function. */
+    "CT1",     /* name of task. */
+    10000,     /* Stack size of task */
+    NULL,      /* parameter of the task */
+    1,         /* priority of the task */
+    &CT1,      /* Task handle to keep track of created task */
+    1          /* pin task to core 0 */
+  ); 
+
+  xTaskCreatePinnedToCore(
+    Task2code,  /* Task function. */
+    "CT2",      /* name of task. */
+    10000,      /* Stack size of task */
+    NULL,       /* parameter of the task */
+    1,          /* priority of the task */
+    &CT2,       /* Task handle to keep track of created task */
+    1           /* pin task to core 0 */      
+  );
+
+    xTaskCreatePinnedToCore(
+    Task3code,  /* Task function. */
+    "Thermo",   /* name of task. */
+    10000,      /* Stack size of task */
+    NULL,       /* parameter of the task */
+    1,          /* priority of the task */
+    &Thermo,    /* Task handle to keep track of created task */
+    1           /* pin task to core 0 */
+  ); 
+
+  xTaskCreatePinnedToCore(
+    Task4code,  /* Task function. */
+    "IR",       /* name of task. */
+    10000,      /* Stack size of task */
+    NULL,       /* parameter of the task */
+    1,          /* priority of the task */
+    &IR,        /* Task handle to keep track of created task */
+    1           /* pin task to core 0 */      
+  );
 
   delay(100);
 }
 
 void Task1code( void * Parameters ){
-  debug("Task1 running on core ");
-  debugln(xPortGetCoreID());
+  // Serial.print("Task1 running on core ");
+  // Serial.println(xPortGetCoreID());
 
   pinMode(CT1_PIN, INPUT); 
 
   // Print available heap memory before allocation
-  // debugf("Free heap before malloc1: %u bytes\n", ESP.getFreeHeap());
+  // Serial.printf("Free heap before malloc1: %u bytes\n", ESP.getFreeHeap());
 
   voltageSamples1 = (float*) malloc(NUM_SAMPLES * sizeof(float));
   if (voltageSamples1 == nullptr) {
-    debugln("Memory1 allocation failed!");
+    Serial.println("Memory1 allocation failed!");
     while (true); // Halt
   }
 
   // Print available heap memory after allocation
-  // debugf("Free heap after malloc1: %u bytes\n", ESP.getFreeHeap());
+  // Serial.printf("Free heap after malloc1: %u bytes\n", ESP.getFreeHeap());
 
   delay(100);
 
@@ -212,37 +250,37 @@ void Task1code( void * Parameters ){
         unsigned long endTime1 = millis();
         unsigned long elapsedTime1 = endTime1 - startTime1;
 
-        debugf("Vavg1: %.3f V, Vrms1: %.3f V, Is1: %.3f A, Ip1: %.3f A, Time1: %lu ms\n", Vavg1, Vrms1, Is1, Ip1, elapsedTime1);
+        // Serial.printf("Vavg1: %.3f V, Vrms1: %.3f V, Is1: %.3f A, Ip1: %.3f A, Time1: %lu ms\n", Vavg1, Vrms1, Is1, Ip1, elapsedTime1);
         
         // Reset sample index to reuse memory
         sampleIndex1 = 0;
 
         // Print available heap memory after one loop
-        // debugf("Free heap after one loop1: %u bytes\n", ESP.getFreeHeap());
+        // Serial.printf("Free heap after one loop1: %u bytes\n", ESP.getFreeHeap());
 
-        delay(500);
+        delay(100);
       }
     }
   }
 }
 
 void Task2code( void * Parameters ){
-  debug("Task2 running on core ");
-  debugln(xPortGetCoreID());
+  // Serial.print("Task2 running on core ");
+  // Serial.println(xPortGetCoreID());
 
   pinMode(CT2_PIN, INPUT);
 
   // Print available heap memory before allocation
-  // debugf("Free heap before malloc2: %u bytes\n", ESP.getFreeHeap());
+  // Serial.printf("Free heap before malloc2: %u bytes\n", ESP.getFreeHeap());
 
   voltageSamples2 = (float*) malloc(NUM_SAMPLES * sizeof(float));
   if (voltageSamples2 == nullptr) {
-    debugln("Memory2 allocation failed!");
+    Serial.println("Memory2 allocation failed!");
     while (true); // Halt
   }
 
   // Print available heap memory after allocation
-  // debugf("Free heap after malloc2: %u bytes\n", ESP.getFreeHeap());
+  // Serial.printf("Free heap after malloc2: %u bytes\n", ESP.getFreeHeap());
 
   delay(100);
 
@@ -302,33 +340,33 @@ void Task2code( void * Parameters ){
         unsigned long endTime2 = millis();
         unsigned long elapsedTime2 = endTime2 - startTime2;
 
-        debugf("Vavg2: %.3f V, Vrms2: %.3f V, Is2: %.3f A, Ip2: %.3f A, Time2: %lu ms\n", Vavg2, Vrms2, Is2, Ip2, elapsedTime2);
+        // Serial.printf("Vavg2: %.3f V, Vrms2: %.3f V, Is2: %.3f A, Ip2: %.3f A, Time2: %lu ms\n", Vavg2, Vrms2, Is2, Ip2, elapsedTime2);
         
         // Reset sample index to reuse memory
         sampleIndex2 = 0;
 
         // Print available heap memory after one loop
-        // debugf("Free heap after one loop2: %u bytes\n", ESP.getFreeHeap());
+        // Serial.printf("Free heap after one loop2: %u bytes\n", ESP.getFreeHeap());
 
-        delay(500);
+        delay(100);
       }
     }
   }
 }
 
 void Task3code( void * Parameters ){
-  debug("Task3 running on core ");
-  debugln(xPortGetCoreID());
+  // Serial.print("Task3 running on core ");
+  // Serial.println(xPortGetCoreID());
 
-  // debugln("MAX6675 Starting......");
+  // Serial.println("MAX6675 Starting......");
   // wait for MAX chip to stabilize
   delay(1000);
 
   // Print available heap memory before allocation
-  // debugf("Free heap before Task3: %u bytes\n", ESP.getFreeHeap());
+  // Serial.printf("Free heap before Task3: %u bytes\n", ESP.getFreeHeap());
 
   for(;;){
-    unsigned long startTime3 = millis(); // Start timing when beginning new sample batch
+    // unsigned long startTime3 = millis(); // Start timing when beginning new sample batch
 
     float temper = thermocouple.readCelsius();
     float temperatureF = thermocouple.readFahrenheit();
@@ -338,37 +376,37 @@ void Task3code( void * Parameters ){
     }
     temperatureC = temper;
 
-    unsigned long endTime3 = millis();
-    unsigned long elapsedTime3 = endTime3 - startTime3;
+    // unsigned long endTime3 = millis();
+    // unsigned long elapsedTime3 = endTime3 - startTime3;
 
-    debugf("KTemperatureC: %.2f °C\n | KTemperatureF: %.2f °C\n | Time3: %lu ms\n", temperatureC, temperatureF, elapsedTime3);
+    // Serial.printf("KTemperatureC: %.2f °C\n | KTemperatureF: %.2f °C\n | Time3: %lu ms\n", temperatureC, temperatureF, elapsedTime3);
 
     // Print available heap memory after one loop
-    // debugf("Free heap after one loop4: %u bytes\n", ESP.getFreeHeap());
+    // Serial.printf("Free heap after one loop4: %u bytes\n", ESP.getFreeHeap());
 
-    delay(1000);
+    delay(100);
   }
 }
 
 void Task4code( void * Parameters ){
-  debug("Task4 running on core ");
-  debugln(xPortGetCoreID());
+  // Serial.print("Task4 running on core ");
+  // Serial.println(xPortGetCoreID());
 
   pinMode(IR_PIN, INPUT);
 
-  // debugln("MD0662 Starting.....");
+  // Serial.println("MD0662 Starting.....");
   // wait for MD chip to stabilize
   delay(1000);
 
   // Print available heap memory before allocation
-  // debugf("Free heap before Task4: %u bytes\n", ESP.getFreeHeap());
+  // Serial.printf("Free heap before Task4: %u bytes\n", ESP.getFreeHeap());
 
   for(;;){
     float irTempSum = 0.0;
     float irVoltage = 0.0;
     int irAdcRawSum = 0;
 
-    unsigned long startTime4 = millis(); // Start timing when beginning new sample batch
+    // unsigned long startTime4 = millis(); // Start timing when beginning new sample batch
 
     for (int i = 0; i < 10; i++) {
       int adc4 = analogRead(IR_PIN);
@@ -382,67 +420,121 @@ void Task4code( void * Parameters ){
     float irAdcRaw = irAdcRawSum / 10.0;
     if (irTemperature < 6.94) irTemperature = 0;
 
-    unsigned long endTime4 = millis();
-    unsigned long elapsedTime4 = endTime4 - startTime4;
+    // unsigned long endTime4 = millis();
+    // unsigned long elapsedTime4 = endTime4 - startTime4;
 
-    debugf("IR Sensor -> ADC: %d | Temperature: %.2f °C\n | Time4: %lu ms\n", irAdcRaw, irTemperature, elapsedTime4);
+    // Serial.printf("IR Sensor -> ADC: %d | Temperature: %.2f °C\n | Time4: %lu ms\n", irAdcRaw, irTemperature, elapsedTime4);
 
     // Print available heap memory after one loop
-    // debugf("Free heap after one loop4: %u bytes\n", ESP.getFreeHeap());
+    // Serial.printf("Free heap after one loop4: %u bytes\n", ESP.getFreeHeap());
 
-    delay(1000);
+    delay(100);
   }
 }
 
-void loop() {
+void loop(){
+  delay(100);
+
   switch (state) {
-    case _WIFI_CONNECT:
-      setupWiFi();
-      if (WiFi.status() == WL_CONNECTED) {
-        setupMQTT();
-        state = _MQTT_CONNECT;
+    case _ON_Init_4G:
+      Serial.println("state - _ON_Init_4G");
+      if (G_start_connect()) {
+        state = _MQTT_START;
+      } else {
+        state = _off_4G;
       }
       break;
 
-    case _MQTT_CONNECT:
-      reconnectMQTT();
-      if (client.connected()) {
-        lastSendTime = millis();
+    case _MQTT_START:
+      Serial.println("state - _MQTT_START");
+      if (IIOT_Dev_kit.MQTT_SETUP(&TB_Broker0, MQTT_SERVER, MQTT_PORT)) {
+        Serial.println("MQTT START");
+        state = _MQTT_Connect;
+      } else {
+        Serial.println("start error");
+        state = _off_4G;
+      }
+      break;
+
+    case _MQTT_Connect:
+      Serial.println("state - _MQTT_Connect");
+      if (IIOT_Dev_kit.MQTT_CONNECT(&TB_Broker0, MQTT_CLIENTID, MQTT_USERNAME)) {
+        Serial.println("Connect to server");
+        state = _send_data;
         error_count = 0;
-        state = _SEND_DATA;
-      }
-      break;
-
-    case _SEND_DATA:
-      if (WiFi.status() != WL_CONNECTED || !client.connected()) {
-        state = _DISCONNECT;
-        break;
-      }
-      client.loop();
-      if (millis() - lastSendTime > sendInterval) {
-        lastSendTime = millis();
-        String payload = "{";
-        payload += "\"currentCT11\": " + String(Ip1, 1) + ",";
-        payload += "\"currentCT12\": " + String(Ip2, 1) + ",";
-        payload += "\"temperatureK\": " + String(temperatureC, 1) + ",";
-        payload += "\"temperatureIR\": " + String(irTemperature, 1);
-        payload += "}";
-        if (client.publish(MQTT_TELE_TOPIC, payload.c_str())) {
-          debugln("Data sent: " + payload);
+        lastSendTime = millis(); // Ensure timing is reset after connection
+      } else {
+        Serial.println("connect error");
+        error_count++;
+        if (error_count >= Error_count_th) {
+          state = _off_4G;
         } else {
-          debugln("Failed to send data");
-          error_count++;
-          if (error_count >= Error_count_th) state = _DISCONNECT;
+          delay(100); // Small delay before retry
+          state = _MQTT_Connect;
         }
       }
       break;
 
-    case _DISCONNECT:
-      WiFi.disconnect();
-      client.disconnect();
+    case _send_data:
+      if (millis() - lastSendTime >= sendInterval) {
+        //Serial.println("state - _send_data");
+        lastSendTime = millis();
+
+        // IIOT_Dev_kit.CSQ();
+
+        float ct1 = Ip1;
+        float ct2 = Ip2;
+        float temp = temperatureC;
+        float irtemp = irTemperature;
+
+        String data = "{";
+        data += "\"currentCT31\": " + String(ct1, 1) + ",";
+        data += "\"currentCT32\": " + String(ct2, 1) + ",";
+        data += "\"temperatureK3\": " + String(temp, 1) + ",";
+        data += "\"temperatureIR3\": " + String(irtemp, 1);
+        data += "}";
+
+        Serial.println("Publishing: " + data);
+        if (IIOT_Dev_kit.MQTT_PUB(&TB_Broker0, MQTT_TELE_TOPIC, data)) {
+          Serial.println("Data sent");
+          error_count = 0;
+        } else {
+          error_count++;
+          if(error_count >= Error_count_th){
+            error_count = 0;
+            Serial.println("Failed to send data");
+            state = _MQTT_Disconnect;
+          }
+        }
+      }
+      break;
+    
+    case _MQTT_Disconnect:
+      Serial.println("state - _MQTT_Disconnect");
+      if (IIOT_Dev_kit.MQTT_DISCONNECT(&TB_Broker0)) {
+        Serial.println("MQTT Disconnect");
+      } else {
+        Serial.println("disconnect error");
+      }
+      state = _MQTT_Stop;
+      break;
+
+    case _MQTT_Stop:
+      Serial.println("state - _MQTT_Stop");
+      if (IIOT_Dev_kit.MQTT_STOP()) {
+        Serial.println("MQTT STOP");
+      } else {
+        Serial.println("MQTT stop error");
+      }
+      state = _off_4G;
+      break;
+
+    case _off_4G:
+      Serial.println("state - _off_4G");
       error_count = 0;
+      IIOT_Dev_kit.PWRDOWN();
       delay(1000);
-      state = _WIFI_CONNECT;
+      state = _ON_Init_4G;
       break;
   }
 }
